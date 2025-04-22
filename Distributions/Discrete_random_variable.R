478416e51c794748557db0465a48c85b66881ed9
Mandana Goudarzi <74295656+mandana-goudarzi@users.noreply.github.com>
2025-04-22 18:42:33 +0200

    final solutions

--- Distributions/Discrete_random_variable.R
+++ Distributions/Discrete_random_variable.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+# Question 1
 # Zero-Truncated Binomial PMF
 dztbinom <- function(k,n,p) {
   if (any(k < 1 | k > n)) {
@@ -23,6 +24,7 @@ cdf <- pztbinom(k_vals, n, p)
 print(pmf)
 print(cdf)
 
+# Question 2
 # Plot of PMF
 pmf_df <- data.frame(k = k_vals, PMF = pmf)
 ggplot(pmf_df, aes(x = k, y = PMF)) + geom_bar(stat="identity") + labs(title = "Zero-Truncated Binomial PMF", x = "k", y = "P(X = k)") + theme_minimal()
@@ -31,6 +33,7 @@ ggplot(pmf_df, aes(x = k, y = PMF)) + geom_bar(stat="identity") + labs(title = "
 cdf_df <- data.frame(k= k_vals, CDF = cdf)
 ggplot(cdf_df, aes(x = k, y = CDF)) + geom_step() + labs(title = "Zero-Truncated Binomial CDF", x = "k", y = "P(X ≤ k)") + theme_minimal()
 
+# Question 3
 # mean
 mean_ztb <- sum(k_vals * pmf)
 
@@ -55,3 +58,15 @@ cat("ZTB Analytical Variance:", var_ztb_theoretical, "\n")
 
 cat("Standard Binomial Mean:", mean_binom, "\n")
 cat("Standard Binomial Variance:", var_binom, "\n")
+
+# Question 4
+sample_size <- 10000
+
+set.seed(42)
+x <- rbinom(2 * sample_size, size= n, prob= p )
+ztb_sample <- x[x>0][1:sample_size]
+
+hist(ztb_sample,breaks = seq(0.5, n + 0.5, 1), main= "Sample from Zero-Truncated Binomial Distribution", xlab= "Number of successes", ylab="Frequency")
+
+sample_mean <- mean(ztb_sample)
+cat("Sample Mean of ZTB:", sample_mean, "\n")
