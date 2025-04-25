7bcfb854a8c60a9784e638d744790a7f444137a4
Mandana Goudarzi <74295656+mandana-goudarzi@users.noreply.github.com>
2025-04-25 10:24:27 +0200

    final

--- Distributions/Web_server_requests.R
+++ Distributions/Web_server_requests.R
@@ -10,3 +10,10 @@ lambda <- 7
 prob_at_least_30 <- 1 - ppois(29, lambda=lambda)
 
 cat("Exact probability P(X ≥ 30) with Poisson(7):", prob_at_least_30, "\n")
+
+# Part c
+var <- 5
+k <- 30 - mean_request
+
+chebyshev_bound <- var/(k^2)
+cat("Chebyshev's inequality upper bound for P(X ≥ 30):", chebyshev_bound, "\n")
