0f93f8f3f4a10bc861b8766a107918473d2bcaf4
Mandana Goudarzi <74295656+mandana-goudarzi@users.noreply.github.com>
2025-05-02 09:57:01 +0200

    part b completed

--- BayesInference/Bayesian_Inference_for_Binomial_model.R
+++ BayesInference/Bayesian_Inference_for_Binomial_model.R
@@ -24,3 +24,20 @@ plot(p_vals, prior_beta, type="l", col="blue", lwd=2, ylab = "Density", xlab = "
 lines(p_vals, likelihood, col= "orange", lwd=2)
 lines(p_vals, post_beta, col= "darkgreen", lwd=2)
 legend("topright", legend = c("Prior", "Likelihood", "Posterior"), col=c("blue", "orange", "darkgreen"), lwd=2)
+
+# Part b
+mode_unif <- (1 + r - 1)/(1 + r + 1 + n - r - 2)
+ci_unif <- qbeta(c(0.025, 0.975), shape1 = 1+r, shape2= 1+n-r)
+
+mode_beta <- (2 + r - 1)/(2+r + 2+n-r - 2)
+ci_beta <- qbeta(c(0.025, 0.975), shape1= 2+r, shape2= 2+n-r)
+
+cat("Flat Prior (Beta(1,1)) → Posterior Beta(14,16)\n")
+cat("  Mode (Most Probable p):", mode_unif,"\n")
+cat("  95% Credible Interval:", ci_unif[1], ci_unif[2], "\n")
+
+cat("Beta Prior (Beta(2,2)) → Posterior Beta(15,17)\n")
+cat("  Mode (Most Probable p):", mode_beta, "\n")
+cat("  95% Credible Interval:", ci_beta[1], ci_beta[2], "\n")
+
+# Part c
